eeeff044c250507b462d082bfe954bb2fa0a19d7
idzn <idzn@yandex.ru>
2017-02-26 13:13:44 +0300

    init

---
+++ app/src/renderer/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="#app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import store from 'renderer/vuex/store'
+  export default {
+    store
+  }
+</script>
+
+<style>
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+</style>
---
+++ app/src/renderer/components/MainPageView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <img src="./MainPageView/assets/logo.png" alt="electron-vue">
+    <h1>Welcome.</h1>
+    <current-page></current-page>
+    <versions></versions>
+    <links></links>
+  </div>
+</template>
+
+<script>
+  import CurrentPage from './MainPageView/CurrentPage'
+  import Links from './MainPageView/Links'
+  import Versions from './MainPageView/Versions'
+  export default {
+    components: {
+      CurrentPage,
+      Links,
+      Versions
+    },
+    name: 'main-page'
+  }
+</script>
+
+<style scoped>
+  img {
+    width: 90px;
+  }
+</style>
